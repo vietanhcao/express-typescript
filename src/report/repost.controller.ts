cbedfda992a19205f8e9fd37cea34718b549e31b
Vietanh <vietanhcao1994@gmail.com>
2022-06-20 22:05:59 +0700

    add aggregation framework

---
+++ src/report/repost.controller.ts
@@ -0,0 +1,76 @@
+import { NextFunction, Response, Router, Request } from "express";
+import userModel from "../users/user.model";
+import { Controller } from "../utils/base-type";
+
+class ReportController implements Controller {
+  public path = "/report";
+  public router = Router();
+  private user = userModel;
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  public initializeRoutes() {
+    this.router.get(this.path, this.generateReport);
+    this.router.get(`${this.path}/count`, this.getNumberOfUser);
+  }
+  private getNumberOfUser = async (request: Request, response: Response, next: NextFunction) => {
+    const numberOfUsersWithAddress = await this.user.countDocuments({
+      address: {
+        $exists: true,
+      },
+    });
+    const countries = await this.user.distinct("address.country", {
+      email: {
+        $regex: /@gmail.com$/,
+      },
+    });
+
+    response.send({ numberOfUsersWithAddress, countries });
+  };
+
+  private generateReport = async (request: Request, response: Response, next: NextFunction) => {
+    const usersByCountry = await this.user.aggregate([
+      {
+        $match: {
+          "address.country": { $exists: true },
+        },
+      },
+      {
+        $group: {
+          _id: { country: "$address.country" },
+          usersLu: {
+            $push: {
+              _id: "$_id",
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        // $lookup: {
+        //   from: "users",
+        //   localField: "usersLu._id",
+        //   foreignField: "_id",
+        //   as: "usersLu", // just name of the field
+        // },
+        $lookup: {
+          from: "posts",
+          localField: "usersLu._id",
+          foreignField: "author",
+          as: "articles", // just name of the field
+        },
+      },
+      {
+        $addFields: { amountOfArticles: { $size: "$articles" } },
+      },
+      {
+        $sort: { amountOfArticles: 1 },
+      },
+    ]);
+    response.send({ usersByCountry });
+  };
+}
+
+export default ReportController;
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import "dotenv/config";
 import validateEnv from "./utils/validateEnv";
 import AuthenticationController from "./authentication/authentication.controller";
 import UserController from "./users/user.controller";
+import ReportController from "./report/repost.controller";
 
 validateEnv();
 
-const app = new App([new AuthenticationController(), new PostsController(), new UserController()], 5000);
+const app = new App([new AuthenticationController(), new PostsController(), new UserController(), new ReportController()], 5000);
 
 app.listen();
